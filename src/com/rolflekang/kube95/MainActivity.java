ca888c1e9655fec8cf9591e99e30b3e6ad999abb
Rolf <relekang@gmail.com>
2011-05-05 22:54:02 +0200

    Added addpant functionality

--- src/com/rolflekang/kube95/MainActivity.java
+++ src/com/rolflekang/kube95/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rolflekang.kube95;
 
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +9,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Button menuPantBtn;
@@ -46,4 +46,5 @@ public class MainActivity extends Activity implements OnClickListener{
 //			break;
 		}
 	}
+	
 }
--- src/com/rolflekang/kube95/PantListActivity.java
+++ src/com/rolflekang/kube95/PantListActivity.java
@@ -2,22 +2,39 @@ package com.rolflekang.kube95;
 
 import java.util.Date;
 
+import android.R.layout;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class PantListActivity extends ListActivity {
+public class PantListActivity extends ListActivity implements OnClickListener {
 	private Pantekassa pantList;
 	private PantAdapter pAdapter;
 	private HttpConnector httpCon;
 	private TextView sumTextField;
+	private Button saveBtn;
+	private EditText amountField, userField;
+	private DatePicker datePicker;
+	private Dialog addDialog;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		addDialog = new Dialog(this);
 		pantList = new Pantekassa();
 		httpCon = new HttpConnector("10.0.1.3");
 		pantList.parseStrings(httpCon.getList(httpCon.PANT));
@@ -35,10 +52,47 @@ public class PantListActivity extends ListActivity {
 			}
 		});
 	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.addmenu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.add:
+	        	addDialog.setContentView(R.layout.addpant);
+	        	addDialog.setTitle("Legg til pant");
+	        	saveBtn = (Button) addDialog.findViewById(R.id.savebtn);
+	        	amountField = (EditText) addDialog.findViewById(R.id.amountfield);
+	        	datePicker = (DatePicker) addDialog.findViewById(R.id.datepicker);
+	        	userField = (EditText) addDialog.findViewById(R.id.userfield);
+	        	saveBtn.setOnClickListener(this);
+	        	addDialog.show();
+	        	break;
+
+	    }
+	    return true;
+	}
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.savebtn:
+			if(httpCon.sendPant(new Date(datePicker.getYear(),datePicker.getMonth(), datePicker.getDayOfMonth()), Double.parseDouble(amountField.getText().toString()), userField.getText().toString()))
+				Toast.makeText(getApplicationContext(), "Panten ble lagt inn", Toast.LENGTH_SHORT );
+			else
+				Toast.makeText(getApplicationContext(), "Det oppstod en feil", Toast.LENGTH_SHORT );
+			addDialog.dismiss();
+			break;
+		}
+	}
 	@SuppressWarnings("unused")
 	private void createTestList() {
 		for (int i = 0; i < 5; i++) { pantList.add(new Pant(new Date(), 200, "Rofl"));}
 		pAdapter.notifyDataSetChanged();
 	}
+	
 
 }
