7f35080cf449955c3e82873835600b484d73f03a
Ole Martin Mork <olemork@gmail.com>
2011-05-05 23:30:18 +0200

    Widget now shows this and nexts weeks cleaner

--- src/com/rolflekang/kube95/WidgetProvider.java
+++ src/com/rolflekang/kube95/WidgetProvider.java
@@ -8,9 +8,11 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider {
-
+	
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
+        
+        CleanGuy cleaner = new CleanGuy();
 
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
@@ -19,10 +21,12 @@ public class WidgetProvider extends AppWidgetProvider {
             // Create an Intent to launch Texty's widget Activity
             Intent intent = new Intent(context, PantListActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
+            
             // Get the layout for the App Widget and attach an on-click listener to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
             views.setOnClickPendingIntent(R.id.widgetpantbtn, pendingIntent);
+            views.setTextViewText(R.id.widgetcurrentcleaner, cleaner.getCleaner());
+            views.setTextViewText(R.id.widgetnextcleaner, cleaner.getNextCleaner());
 
             // Tell the AppWidgetManager to perform an update on the current App Widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
