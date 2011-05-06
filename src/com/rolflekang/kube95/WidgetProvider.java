80aa19f03b5a0aaa846180962bb1624ede5bb7a6
Ole Martin Mork <olemork@gmail.com>
2011-05-06 17:48:55 +0200

    Widget update function finished

--- src/com/rolflekang/kube95/WidgetProvider.java
+++ src/com/rolflekang/kube95/WidgetProvider.java
@@ -3,6 +3,7 @@ package com.rolflekang.kube95;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -15,6 +16,7 @@ public class WidgetProvider extends AppWidgetProvider {
 	private HttpConnector httpCon;
 	private Pantekassa pantList;
 	
+	@Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
         
@@ -48,9 +50,15 @@ public class WidgetProvider extends AppWidgetProvider {
     }
     
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent) {    	
     	if(intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
-    		Toast.makeText(context, "Oppdaterte sum pant", Toast.LENGTH_SHORT).show();
+    		
+    		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+    	    int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
+    	    if (appWidgetIds.length > 0) {
+    	        new WidgetProvider().onUpdate(context, appWidgetManager, appWidgetIds);
+        		Toast.makeText(context, "Oppdaterte pantesum", Toast.LENGTH_SHORT).show();
+    	    }    	    
     	}
     	
     	super.onReceive(context, intent);
