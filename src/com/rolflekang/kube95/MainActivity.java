c44a5622f024070e226842781a7ed1b4af7263d6
Rolf <relekang@gmail.com>
2011-05-06 21:00:12 +0200

    Added deleteusername to mainmenu

--- src/com/rolflekang/kube95/MainActivity.java
+++ src/com/rolflekang/kube95/MainActivity.java
@@ -6,13 +6,13 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Button menuPantBtn, changeCleanerButton, saveUserNameButton;
@@ -50,6 +50,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		userNameDialog = new Dialog(this);
 		userNameDialog.setContentView(R.layout.usernamedialog);
 		userNameDialog.setTitle("Legg inn navnet ditt");
+		userNameDialog.setCancelable(false);
 		userNameField = (EditText) userNameDialog.findViewById(R.id.usernamefield);
 		userNameField.setInputType(EditorInfo.TYPE_TEXT_FLAG_CAP_WORDS);
 		saveUserNameButton = (Button) userNameDialog.findViewById(R.id.usernamesavebtn);
@@ -69,9 +70,17 @@ public class MainActivity extends Activity implements OnClickListener{
 			startActivityForResult(i1, 0);
 			break;
 		case R.id.usernamesavebtn:
+			if(userNameField.getText().toString() == ""){
+				Toast.makeText(getApplicationContext(), "Du må skrive inn navn", Toast.LENGTH_LONG);
+				break;
+			}
 			settings.setUserName(userNameField.getText().toString());
-			nextCleanWeekTextView.setText(cleanGuy.getNextCleanWeek(userNameField.getText().toString()));
+			nextCleanWeekTextView.setText(Integer.toString(cleanGuy.getNextCleanWeek(userNameField.getText().toString())));
 			userNameDialog.dismiss();
+			break;
+		case R.id.menu_deleteusernamebtn:
+			settings.deleteUserName();
+			viewUserNameDialog();
 			break;
 //		case R.id.feedbackbtn:
 //			Toast.makeText(this, "...", Toast.LENGTH_SHORT);
