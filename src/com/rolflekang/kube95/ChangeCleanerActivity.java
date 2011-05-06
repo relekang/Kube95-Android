5af1a236a20c61bc447c41cfeb3cddbeadd422b7
Rolf <relekang@gmail.com>
2011-05-06 19:20:34 +0200

    Added next cleanweek functionality. No backend save as of now.

--- src/com/rolflekang/kube95/ChangeCleanerActivity.java
+++ src/com/rolflekang/kube95/ChangeCleanerActivity.java
@@ -1,12 +1,54 @@
 package com.rolflekang.kube95;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Calendar;
 
-public class ChangeCleanerActivity extends Activity{
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+public class ChangeCleanerActivity extends Activity implements OnClickListener{
+	private CleanGuy cleanGuy;
+	private Settings settings;
+	private TextView curentWeekView;
+	private Spinner newWeekSelector;
+	private Button saveButton;
+	private ArrayAdapter<String> spinnerAdapter;
+	private ArrayList<String> nextList;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.changecleaner);
+		
+		cleanGuy = new CleanGuy();
+		settings = new Settings();
+		curentWeekView = (TextView) this.findViewById(R.id.changecleaner_week);
+		newWeekSelector = (Spinner) this.findViewById(R.id.changecleaner_newweekselector);
+		saveButton = (Button) this.findViewById(R.id.changecleaner_savebtn);
+		
+		Calendar cal = Calendar.getInstance();
+//		String week = "Bytt fra uke " + cal.get(Calendar.WEEK_OF_YEAR) + " til";
+		String week = "Bytt fra uke " + cleanGuy.getNextCleanWeek(settings.getUserName()) + " til";
+		curentWeekView.setText(week);
+		
+//		nextList = cleanGuy.getNextCleanersList(cal.get(Calendar.WEEK_OF_YEAR));
+		nextList = cleanGuy.getNextCleanersList(cleanGuy.getNextCleanWeek(settings.getUserName()));
+		spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item , nextList);
+		newWeekSelector.setAdapter(spinnerAdapter);
+		
+		saveButton.setOnClickListener(this);
+		
+	}
+	@Override
+	public void onClick(View v) {
+			//TODO:
 	}
 }
--- src/com/rolflekang/kube95/CleanGuy.java
+++ src/com/rolflekang/kube95/CleanGuy.java
@@ -1,23 +1,32 @@
 package com.rolflekang.kube95;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 
 public class CleanGuy {
+	private final int KATRINE = 9;
+	private final int ROLFOLE = 10;
+	private final int JULIE = 11;
+	private final int ROLFERIK = 12;
+	private final int ANDERS = 13;
+	private final int ANDREAS = 14;
+	private final int OLE = 15;
+	private final int HAVARD = 16;
 		
 	public String getCleaner() {
 		Calendar c = Calendar.getInstance();
 		return getCleaner(c.get(Calendar.WEEK_OF_MONTH));
 	}
 	public String getCleaner(int weeknr) {
-		if (weeknr == 9 || (weeknr - 9) % 8 == 0) return "Katrine";
-		else if(weeknr == 10 || (weeknr - 10) % 8 == 0) return "Rolf";
-		else if(weeknr == 11 || (weeknr - 11) % 8 == 0) return "Julie";
-		else if(weeknr == 12 || (weeknr - 12) % 8 == 0)  return "Rolf Erik";
-		else if(weeknr == 13 || (weeknr - 13) % 8 == 0)  return "Anders";
-		else if(weeknr == 14 || (weeknr - 14) % 8 == 0)  return "Andreas";
-		else if(weeknr == 15 || (weeknr - 15) % 8 == 0)  return "Ole";
-		else if(weeknr == 16 || (weeknr - 16) % 8 == 0)  return "Håvard";
+		if (weeknr == KATRINE || (weeknr - KATRINE) % 8 == 0) return "Katrine";
+		else if(weeknr == ROLFOLE || (weeknr - ROLFOLE) % 8 == 0) return "Rolf";
+		else if(weeknr == JULIE || (weeknr - JULIE) % 8 == 0) return "Julie";
+		else if(weeknr == ROLFERIK || (weeknr - ROLFERIK) % 8 == 0)  return "Rolf Erik";
+		else if(weeknr == ANDERS || (weeknr - ANDERS) % 8 == 0)  return "Anders";
+		else if(weeknr == ANDREAS || (weeknr - ANDREAS) % 8 == 0)  return "Andreas";
+		else if(weeknr == OLE || (weeknr - OLE) % 8 == 0)  return "Ole";
+		else if(weeknr == HAVARD || (weeknr - HAVARD) % 8 == 0)  return "Havard";
 		return "Random";
 	}
 	public String getNextCleaner() {
@@ -25,8 +34,23 @@ public class CleanGuy {
 		return getCleaner(c.get(Calendar.WEEK_OF_MONTH) + 1);
 	}
 	public int getNextCleanWeek(String name){
-		//TODO: Lage denne funskjonen
+		//TODO: Only deliveres first week. Has to calculate next..
+		if(name.equalsIgnoreCase("katrine")) return KATRINE;
+		else if(name.equalsIgnoreCase("rolfole")) return ROLFOLE;
+		else if(name.equalsIgnoreCase("julie")) return JULIE;
+		else if(name.equalsIgnoreCase("rolferik")) return ROLFERIK;
+		else if(name.equalsIgnoreCase("anders")) return ANDERS;
+		else if(name.equalsIgnoreCase("andreas")) return ANDREAS;
+		else if(name.equalsIgnoreCase("ole")) return OLE;
+		else if(name.equalsIgnoreCase("havard"))  return HAVARD;
 		return 0;
+	}
+	public ArrayList<String> getNextCleanersList(int weeknr) {
+		ArrayList<String> list = new ArrayList<String>();
+		for (int i = 1; i < 8; i++) {
+			list.add((weeknr + i) +" - "+ getCleaner(weeknr + i));			
+		}
+		return list;
 	}
 
 }
