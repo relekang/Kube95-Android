d42beb66e4aa565aac3d674145108858f308cc6a
Rolf <relekang@gmail.com>
2011-05-06 23:36:29 +0200

    Removde unused imports

--- src/com/rolflekang/kube95/ChangeCleanerActivity.java
+++ src/com/rolflekang/kube95/ChangeCleanerActivity.java
@@ -1,18 +1,13 @@
 package com.rolflekang.kube95;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class ChangeCleanerActivity extends Activity implements OnClickListener{
