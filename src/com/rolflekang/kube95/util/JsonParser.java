69ae7539e988582ba4aadfe053e2b71a440986dc
Rolf <relekang@gmail.com>
2011-05-10 21:14:06 +0200

    Code refactoring

---
+++ src/com/rolflekang/kube95/util/JsonParser.java
@@ -0,0 +1,45 @@
+package com.rolflekang.kube95.util;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.rolflekang.kube95.Pant;
+
+public class JsonParser {
+	
+	public ArrayList<Pant> parsePantekassa(String list) throws JSONException {
+		ArrayList<Pant> tmp = new ArrayList<Pant>();
+			JSONArray jsonArray = new JSONArray(list);
+			Calendar cal = Calendar.getInstance();
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject json = jsonArray.getJSONObject(i);
+				String[] d = json.getString("date").split("-");
+				String user = json.getString("user");
+				user = new JSONObject(user).getString("username");
+				cal.set(Integer.parseInt(d[0]), Integer.parseInt(d[1]), Integer.parseInt(d[2]));
+				tmp.add(new Pant(cal.getTime(), Double.parseDouble(json.getString("amount")), user));				
+			}
+		return tmp;
+	}
+	public int[][] parseSwaps(String jsonString) throws JSONException {
+		JSONArray jArray = new JSONArray(jsonString);
+		int[][] swaps = new int[jArray.length()][2];
+		for (int i = 0; i < jArray.length(); i++) {
+			JSONObject jObject = jArray.getJSONObject(i);
+			swaps[i][0] = jObject.getInt("oldweek");
+			swaps[i][1] = jObject.getInt("newweek");
+		}
+		return swaps;
+	}
+	public JSONObject createSwap(int oldWeekNr, int newWeekNr) throws JSONException {
+		JSONObject jObject = new JSONObject();
+		jObject.put("oldweek", oldWeekNr);
+		jObject.put("newweek", newWeekNr);
+		return jObject;
+	}
+
+}
---
+++ src/com/rolflekang/kube95/util/Settings.java
@@ -0,0 +1,37 @@
+package com.rolflekang.kube95.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+
+public class Settings {
+	private final String NODETITLEUSERNAME = "username";
+	
+	private SharedPreferences pref;
+	
+	public Settings(Context context) {
+		pref = PreferenceManager.getDefaultSharedPreferences(context);
+	}
+	public String getUserName() {
+		String name = pref.getString(NODETITLEUSERNAME, "Name");
+		if(!(name == "Name")) return name;
+		else return null;
+		
+	}
+	public void setUserName(String username) {
+		Editor editor = pref.edit();
+		editor.putString(NODETITLEUSERNAME, username);
+		editor.commit();
+	}
+	public boolean isUserNameSet(){
+			if(pref.contains(NODETITLEUSERNAME)) return true;
+		return false;
+	}
+	public void deleteUserName(){
+		pref.edit().remove(NODETITLEUSERNAME);
+	}
+	public void deleteAll(){	
+		pref.edit().clear().commit();
+	}
+}
